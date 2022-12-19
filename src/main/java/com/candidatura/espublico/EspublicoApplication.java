98900f3325da7a0a44e3e7a20727aa666c728472
mig21bb <mig21bb@gmail.com>
2022-12-19 11:40:58 +0100

    first commit

---
+++ src/main/java/com/candidatura/espublico/EspublicoApplication.java
@@ -0,0 +1,13 @@
+package com.candidatura.espublico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EspublicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EspublicoApplication.class, args);
+	}
+
+}
